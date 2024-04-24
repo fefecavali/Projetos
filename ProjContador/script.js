588bcf0db22b713ebb69ef8a0c8c6a551fb4ac88
Fernanda Cavali <fefecavali@gmail.com>
2024-04-24 11:35:50 -0300

    Ajuste

--- ProjContador/script.js
+++ ProjContador/script.js
@@ -5,37 +5,28 @@ const divContador = document.querySelector("[txt-contador]")
 let contagemCaracteres = true
 
 function contagem () {
-    let quantPalavras = 0
-    if (contagemCaracteres) {
-    let isPalavra = false;
-    for (let i = 0; i < inputTexto.value.length; i++) {
-        if (inputTexto.value[i] !== " " && inputTexto.value[i] !== "\n" && inputTexto.value[i] !== "\t") // verificando se nn tem é espaco em branco, quebra linha ou tab
-         {          
-            if (!isPalavra) {
-                isPalavra = true;
-                quantPalavras++;
-            }
-        } else {
-            isPalavra = false;
-    divContador.innerText = quantPalavras; 
-                } 
-            }
-        } else{
-        let quantCaractere = inputTexto.value.length
-        divContador.innerText = quantCaractere; }
-       
+    const text = inputTexto.value
+    if (contagemCaracteres) { 
+        const countWords = text.trim().split(" ") ;
+        divContador.innerText = countWords.length + " palavras";
+    }else{  
+        let quantCaractere = text.length
+        divContador.innerText = quantCaractere + " caracteres"; }
 }
 
-// "&&" verdadeiro ao mesmo tempo
 
 
 function handleBtnContar(evento) {
+    divContador.innerText = 0;
+    
     if (contagemCaracteres){
         evento.target.innerText = "Contar Palavras"     
         contagemCaracteres = false;
+        contagem();
     } else {
         evento.target.innerText = "Contar Caracteres"
         contagemCaracteres = true ;
+        contagem();
     }
 }
 // variavel q recebeu função não declarativa

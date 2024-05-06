c982225b117627b1e747425793cf1383eb409bd2
Fernanda Cavali <fefecavali@gmail.com>
2024-05-06 00:39:02 -0300

    Adicionando comentários p Aula

--- ProjGeradorLink/script.js
+++ ProjGeradorLink/script.js
@@ -1,29 +1,21 @@
 let input = document.querySelector("#input") ;
 
-// const regex = /(\d{2})(\d{4,5})(\d{4})/;
-
 const regex = /(\(?\d{2})(\d{4,5})(\d{4})/g;
 
 input.addEventListener("input", function() {
-
     // Permite somente dígitos
-    let cleanedPhone = input.value.replace(/\D/g, '')
-    
-    let newPhone = cleanedPhone.replace(regex, '($1) $2-$3') 
+    let cleanedPhone = input.value.replace(/\D/g, '') ; 
+    let newPhone = cleanedPhone.replace(regex, '($1) $2-$3') ; //Adicionando máscara
+
+    // Temporário pq vou colocar nos botões -->
 
     if (cleanedPhone.length > 11) {
         alert("ERROR. O número digitado possui mais de 11 dígitos.");
     } else {
         input.style.color = 'green';
-    }
+    } ;
+
+    console.log(input.value) //verificando
     
-    // Máscara (xx) xxxx-xxxx
-    input.value = newPhone;
-    console.log(input.value)
-        
-          
+    input.value = newPhone;        
 });
-            // if () {
-            //         input.style.color = 'green'
-            //     }
-                

eca63a0dfea0cbe7a7df686cf2ea6dc1833295de
Fernanda Cavali <fefecavali@gmail.com>
2024-05-06 00:32:39 -0300

    Adicinando máscara ao número

--- ProjGeradorLink/script.js
+++ ProjGeradorLink/script.js
@@ -1,10 +1,29 @@
 let input = document.querySelector("#input") ;
 
-const regex = /(\(?\d{2}\)?\s)?(\d{4,5}\-\d{4})/g;
+// const regex = /(\d{2})(\d{4,5})(\d{4})/;
 
+const regex = /(\(?\d{2})(\d{4,5})(\d{4})/g;
 
 input.addEventListener("input", function() {
-    // Permite somente dÍgitos
-    input.value = input.value.replace(/[^0-9]/g, '')
 
-})
+    // Permite somente dígitos
+    let cleanedPhone = input.value.replace(/\D/g, '')
+    
+    let newPhone = cleanedPhone.replace(regex, '($1) $2-$3') 
+
+    if (cleanedPhone.length > 11) {
+        alert("ERROR. O número digitado possui mais de 11 dígitos.");
+    } else {
+        input.style.color = 'green';
+    }
+    
+    // Máscara (xx) xxxx-xxxx
+    input.value = newPhone;
+    console.log(input.value)
+        
+          
+});
+            // if () {
+            //         input.style.color = 'green'
+            //     }
+                

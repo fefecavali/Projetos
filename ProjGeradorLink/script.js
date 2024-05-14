ce6de7004792d1e0b74bc107e73f1523806d2b15
Fernanda Cavali <fefecavali@gmail.com>
2024-05-14 20:53:43 -0300

    Adicionando função de gerar link e arrumando alguns bugs

--- ProjGeradorLink/script.js
+++ ProjGeradorLink/script.js
@@ -1,33 +1,40 @@
-let input = document.querySelector("#input") ;
-let sendBtn = document.querySelector("#sendBtn") ;
-let copyBtn = document.querySelector("#copyBtn") ;
+const input = document.querySelector("#input") ;
+const sendBtn = document.querySelector("#sendBtn") ;
+const copyBtn = document.querySelector("#copyBtn") ;
 
-const regex = /(\(?\d{2})(\d{4,5})(\d{4})/g ;
+const regex = /(\(?\d{2})(\d{4,5})(\d{4})/g ;  //expressao regular
+
+// Funçõo que vai gerar o link 
+let linkWhatsapp = () => { 
+    let clearNumber = input.value.replace(/\D/g, ''); // Remover todos os caracteres que não são dígitos
+    return `https://wa.me/55${clearNumber}`;
+    
+}
 
 
-function handleBtnContar() {
-    console.log('oi')
+function copytoClipboard() {
+    // navigator.clipboard.whiteText(linkWhatsapp)
+    console.log(linkWhatsapp())
 }
 
 input.addEventListener("input", function(event) {
     // Permite somente dígitos
     let cleanedPhone = input.value.replace(/\D/g, '') ; 
     let newPhone = cleanedPhone.replace(regex, '($1) $2-$3') ; //Adicionando máscara
-
-    // Temporário pq vou colocar nos botões -->
-
+    
     if (cleanedPhone.length > 11) {
         input.style.color = 'black';
         input.value = newPhone.slice(0, 14); 
         alert("ERROR. O número só pode possuir até 11 dígitos.");
         
-        
+    
     } else if (cleanedPhone.length < 11) {
         input.style.color = 'black';
         input.value = newPhone; 
     } else if (cleanedPhone.length === 11) {
         input.style.color = 'green';
-        sendBtn.addEventListener("click", handleBtnContar() )
+        // sendBtn.addEventListener("click", copytoClipboard())
+        copyBtn.addEventListener("click", copytoClipboard())
     };
     
 });

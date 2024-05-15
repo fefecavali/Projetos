40beb15da139f267ef51042ad85bb3be2fe548f0
Fernanda Cavali <fefecavali@gmail.com>
2024-05-15 00:38:26 -0300

    Arrumando bugs pt2 e configurando segundo botão

--- ProjGeradorLink/script.js
+++ ProjGeradorLink/script.js
@@ -1,40 +1,52 @@
-const input = document.querySelector("#input") ;
-const sendBtn = document.querySelector("#sendBtn") ;
-const copyBtn = document.querySelector("#copyBtn") ;
+// Variáveis -> 
+let input = document.querySelector("#input") ;
+let copyBtn = document.querySelector("#copyBtn") ;
+let sendBtn = document.querySelector("#sendBtn") ;
+let resultLink = document.querySelector(".resultLink") ;
 
-const regex = /(\(?\d{2})(\d{4,5})(\d{4})/g ;  //expressao regular
+const regex = /(\(?\d{2})(\d{4,5})(\d{4})/g ;  // expressão regular
 
-// Funçõo que vai gerar o link 
+// Funçõo que vai gerar o link ->
 let linkWhatsapp = () => { 
-    let clearNumber = input.value.replace(/\D/g, ''); // Remover todos os caracteres que não são dígitos
+    let clearNumber = input.value.replace(/\D/g, ''); 
     return `https://wa.me/55${clearNumber}`;
     
 }
 
-
-function copytoClipboard() {
-    // navigator.clipboard.whiteText(linkWhatsapp)
-    console.log(linkWhatsapp())
+function messageLink(evento) {
+    evento.target.innerHTML = "Clique no link para copiar" 
 }
 
-input.addEventListener("input", function(event) {
-    // Permite somente dígitos
-    let cleanedPhone = input.value.replace(/\D/g, '') ; 
-    let newPhone = cleanedPhone.replace(regex, '($1) $2-$3') ; //Adicionando máscara
+// Permite a entrada de até 11 digitos, realiza a mascara quando o numero é validado e muda a cor ->
+input.addEventListener("input", function() {
     
-    if (cleanedPhone.length > 11) {
-        input.style.color = 'black';
-        input.value = newPhone.slice(0, 14); 
-        alert("ERROR. O número só pode possuir até 11 dígitos.");
-        
-    
-    } else if (cleanedPhone.length < 11) {
-        input.style.color = 'black';
-        input.value = newPhone; 
-    } else if (cleanedPhone.length === 11) {
+    input.value = input.value.replace(/\D/g, '') ;  // Permite somente dígitos       
+
+    if (input.value.length == 11){
         input.style.color = 'green';
-        // sendBtn.addEventListener("click", copytoClipboard())
-        copyBtn.addEventListener("click", copytoClipboard())
-    };
+        input.value = input.value.replace(regex, '($1) $2-$3') ; //Adicionando máscara    
+        
+    } else if (input.value.length > 11) {
+        alert("ERROR. O número só pode possuir até 11 dígitos.");
+        input.value = input.value.substring(0,11);  //barra a entrada de mais digitos uma vez que foram atingidos os 11
+        
+    } else if (input.value.length < 11) {
+        input.style.color = 'red';
+    }
     
 });
+
+// Volta a tela inicial
+input.addEventListener("click", ()=> {resultLink = ""})
+
+//Primeiro botao gera link e o exibe logo abaixo ->
+copyBtn.addEventListener("click", function () {
+    // copyBtn.addEventListener("click", function() {
+        //     navigator.clipboard.writeText(linkWhatsapp()) ;
+        //     console.log(linkWhatsapp()) ;
+        // });
+    
+})
+
+// Segundo botao gera link e abre em uma nova aba ->
+sendBtn.addEventListener("click", () => {window.open(linkWhatsapp())})

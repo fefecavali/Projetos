716f8e2df89f79a3bea94cff2a67373f47d0b052
Fernanda Cavali <fefecavali@gmail.com>
2024-05-30 00:01:42 -0300

    Ajustando

---  ProjListaTarefas/script.js
+++  ProjListaTarefas/script.js
@@ -1,23 +1,35 @@
 let btn = document.querySelector("button")
 let taskVar = document.querySelector("article")
-let input = document.querySelector("input")
+let input = document.querySelector("#addNewTask")
 let counterDone = document.querySelector("#doneCounter")
 let counterTask = document.querySelector("#taskCounter")
+let messageParagraph = document.querySelector("#inicialMessage");
 
+let counter = 0
 
 btn.addEventListener("click", ()=> {
     if (input.value.trim() === '') {
         alert("Digite uma tarefa!");
         return;
+    } else {
+        messageParagraph.innerHTML = '';
     }
 
     createTask()
-     
 })
 
 function createTask() {
-    let li = document.createElement("li")
-   li.textContent = input;
-            ul.appendChild(taskItem);
-            taskInput.value = "";
+    let divTask = document.createElement("div");
+    let list = document.createElement("ul"); // Criando a lista de tarefas
+    let itemList = document.createElement("li");
+    itemList.textContent = input.value;
+
+    list.appendChild(itemList);
+    divTask.appendChild(list);
+
+    // Adicionando classe 
+    divTask.classList.add("task");
+    taskVar.appendChild(divTask);
+    input.value = '';            
+    
 }
